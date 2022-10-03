1cb9b353c223e05ca9107da7434be4592bfac9bf
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-03 21:06:25 +0300

    hw#7 task1

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryBox = document.querySelector(".gallery");
+
+createAndRenderGallery(galleryItems, galleryBox);
+
+galleryBox.addEventListener("click", getBigImgUrl);
+
+function createAndRenderGallery(galleryItems, galleryBox) {
+  const galleryLi = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+  galleryBox.insertAdjacentHTML("beforebegin", galleryLi);
+}
+
+function getBigImgUrl(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const bigImgUrl = evt.target.dataset.source;
+  toOpenModal(bigImgUrl);
+}
+let instance = null;
+function toOpenModal(ImgUrl) {
+  instance = basicLightbox.create(`
+    <img src="${ImgUrl}" width="1000" height="800">
+`);
+  window.addEventListener("keydown", toCloseModal);
+  instance.show();
+}
+function toCloseModal(evt) {
+  if (evt.code === "Escape") {
+    instance.close();
+    window.addEventListener("keydown", toCloseModal);
+  }
+}
