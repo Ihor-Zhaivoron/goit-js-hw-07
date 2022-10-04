1330d25767b5808da0f284487d15832cdfc240ce
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-04 13:03:36 +0300

    hw37 task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryBox = document.querySelector(".gallery");
 
-console.log(galleryItems);
+createGallery(galleryItems, galleryBox);
+
+function createGallery(galleryItems, galleryBox) {
+  const galleryLi = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+  galleryBox.innerHTML = galleryLi;
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
